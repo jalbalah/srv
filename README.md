29905009983c7fd6b662518ef03d3aa7d4a741fc
Jason Albalah <jason.albalah@yahoo.com>
2018-10-27 16:09:54 -0400

    update

--- README.md
+++ README.md
@@ -12,8 +12,9 @@ python srv.py
 ###Step 2: Create 
 ```
 srv --list
-srv --install my_service
-srv --list
+srv --install my_service --version 1.0
+srv -i my_svc -v 2
+srv --l
 ```
 
 ###Step 3: Start
--- srv.py
+++ srv.py
@@ -8,25 +8,43 @@ import argparse
 
 if __name__ == '__main__':
     DEPLOY_DIR = os.path.join(os.getcwd(), 'deploy')
-    RUN_FILE = ('run.sh', "echo hi")
+    deploy = lambda x: os.path.join(DEPLOY_DIR, x)
+    version_missing = lambda: \
+        True if 'version' not in args or not args.version else False
+    RUN_FILE = ('run.sh', "python main.py")
     parser = argparse.ArgumentParser(__name__)
 
     parser.add_argument('-i', '--install',
                         type=str,
                         help='install service name')
+    parser.add_argument('-v', '--version',
+                        type=float,
+                        help='version for service name')
     parser.add_argument('-l', '--list',
                         action="store_true",
                         help='show services')
+    parser.add_argument('-f', '--files',
+                        help='show files for service name')
     parser.add_argument('-s', '--start',
                         help='start service name')
+    parser.add_argument('-c', '--clean',
+                        help='clean one or "ALL" services')
+    parser.add_argument('-d', '--deploy',
+                        help='set the active service version')
 
     args = parser.parse_args()
     if args.install:
-        dsn = os.path.join(DEPLOY_DIR, args.install)
+        if version_missing():
+            raise Exception('Missing version number')
+        dsn = os.path.join(deploy(args.install), str(args.version))
         os.system('rm -rf {} && mkdir {}'.format(dsn, dsn))
         dsn2 = os.path.join(dsn, RUN_FILE[0])
         with open(dsn2, 'w', encoding='utf-8') as wf:
             wf.write(RUN_FILE[1])
+        dsn3 = os.path.join(dsn, 'main.py') # to do VERSIONING
+        with open(dsn3, 'w') as wf:
+            with open('main.py') as rf:
+                wf.write(rf.read())
         print('Installed {}'.format(args.install))
     elif args.list:
         try:
@@ -35,7 +53,26 @@ if __name__ == '__main__':
         except FileNotFoundError:
             print('No services exist')
     elif args.start:
-        path = os.path.join(DEPLOY_DIR, args.start, RUN_FILE[0])
+        path = deploy(args.start, RUN_FILE[0])
         res = os.system('bash {}'.format(path))
+    elif args.files:
+        path = deploy(args.files)
+        os.system('ls -a --color=auto {}'.format(path))
+    elif args.clean:
+        if args.clean.strip().upper() == 'ALL':
+            rsp = input('Should you remove all files?! yes/no:  ')
+            if rsp.strip() == 'yes':
+                os.system('rm -rf {}'.format(DEPLOY_DIR))
+                print('All files removed!')
+            else:
+                print('Operation canceled :)')
+        else:
+            if version_missing():
+                raise Exception('Missing version number')
+            path = deploy(args.clean, args.version)
+            os.system('rm -rf {}'.format(path))
+    elif args.deploy:
+
+        path = deploy()
     else:
         print('Try --help')
